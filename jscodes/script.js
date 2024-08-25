442a0f9274129507cf91c5d788de392eb8e82926
JagtapSiddhant11 <siddhantjagtap44@gmail.com>
2024-08-25 17:35:09 +0530

    first commit of javascript

---
+++ jscodes/script.js
@@ -0,0 +1,11 @@
+
+    const Fruit = {
+        name : 'Apple',
+        weight : 500,
+        nutritious: true,
+        location : 'jammu & kashmir'
+
+    };
+    for(const i in Fruit){
+        console.log(Fruit[i]);
+    }
