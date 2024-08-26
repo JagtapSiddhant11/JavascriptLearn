f3a0f3212d6f2c36010354fdcc2e1524f73afef7
JagtapSiddhant11 <siddhantjagtap44@gmail.com>
2024-08-26 12:36:18 +0530

    javascript variables and datatypes

---
+++ jscodes/01_Variables.js
@@ -0,0 +1,20 @@
+const accountId = 14453;
+let accountEmail = "abc@gmail.com";
+var accountPassword = 12345;
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 2;
+
+accountEmail = 'hscsf@gmail.com'
+accountPassword = 244443;
+accountCity = 'bengluru';
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scop
+
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+console.log(accountId);
---
+++ jscodes/02_Datatypes.js
@@ -0,0 +1,11 @@
+// alert(3 + 4) // we are using nodejs, not browser
+
+//null => standalone value
+//undefined => value yet not defined
+//symbol => unique
+
+
+
+//object 
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
